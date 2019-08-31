0560acb8f425cc28604a98e95f76549613b9f4c5
Andrew Gaynor <andrewgaynor@Andrews-MacBook-Pro.local>
2019-08-31 14:28:04 -0400

    Updates

--- scripts/javascript.js
+++
@@ -1,8 +0,0 @@
-const expandButton  = document.getElementById('expand-button');
-
-expandButton.onclick = function() {
-    const mobileNav = document.querySelectorAll('.mobile-nav');
-    for(var i = 0; i < mobileNav.length; i++) {
-        mobileNav[i].classList.toggle('open');
-    }
-};
